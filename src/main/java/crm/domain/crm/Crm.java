b42729900ddb02e9c426ff2167bf13515d39bbc7
Elena Piaggio <elenapiaggio@gmail.com>
2022-07-16 17:15:10 +0200

    ADD newLead method

--- src/main/java/crm/domain/crm/Crm.java
+++ src/main/java/crm/domain/crm/Crm.java
@@ -5,12 +5,15 @@ import crm.domain.Lead.Lead;
 import crm.domain.Opportunity.Opportunity;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Crm {
 
   private ArrayList<Lead> leadsList;
   private ArrayList<Opportunity> opportunitiesList;
   private ArrayList<Company> companiesList;
+
+  Scanner scanner = new Scanner(System.in);
 
   public Crm() {
   }
@@ -32,4 +35,22 @@ public class Crm {
   public ArrayList<Company> getCompaniesList() {
     return companiesList;
   }
+
+  public void newLead(){
+    System.out.println("Name: ");
+    String name = scanner.next();
+
+    System.out.println("Phone: ");
+    String phone = "";
+
+    System.out.println("Email: ");
+    String email = "";
+
+    System.out.println("Company: ");
+    String company = "";
+
+    Lead oneLead = new Lead(name, phone, email, company);
+    leadsList.add(oneLead);
+  }
+
 }
