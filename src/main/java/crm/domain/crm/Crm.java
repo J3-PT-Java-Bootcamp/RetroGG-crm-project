5d4fe777c923b418a871fb3a015b9a8a8cc31b6c
Elena Piaggio <elenapiaggio@gmail.com>
2022-07-16 16:59:10 +0200

    ADD class atributes

---
+++ src/main/java/crm/domain/crm/Crm.java
@@ -0,0 +1,17 @@
+package crm.domain.crm;
+
+import crm.domain.Company.Company;
+import crm.domain.Lead.Lead;
+import crm.domain.Opportunity.Opportunity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crm {
+
+  private ArrayList<Lead> leadsList;
+  private ArrayList<Opportunity> opportunitiesList;
+  private ArrayList<Company> companiesList;
+
+
+}
